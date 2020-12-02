00580566cd390c40900a854a59ec5f369fe39866
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2020-12-02 14:15:42 +0900

    feat(*): add Coordinator client

--- client_test.go
+++ client_test.go
@@ -2,63 +2,84 @@ package godruid
 
 import (
 	"fmt"
-	. "github.com/smartystreets/goconvey/convey"
+	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGroupby(t *testing.T) {
-	Convey("TestGroupby", t, func() {
-		query := &QueryGroupBy{
-			DataSource:   "campaign",
-			Intervals:    []string{"2014-09-01T00:00/2020-01-01T00"},
-			Granularity:  GranAll,
-			Filter:       FilterAnd(FilterJavaScript("hour", "function(x) { return(x >= 1) }"), nil),
-			LimitSpec:    LimitDefault(5),
-			Dimensions:   []DimSpec{"campaign_id"},
-			Aggregations: []Aggregation{AggRawJson(`{ "type" : "count", "name" : "count" }`), AggLongSum("impressions", "impressions")},
-			PostAggregations: []PostAggregation{PostAggArithmetic("imp/count", "/", []PostAggregation{
-				PostAggFieldAccessor("impressions"),
-				PostAggRawJson(`{ "type" : "fieldAccess", "fieldName" : "count" }`)})},
-		}
-		client := Client{
-			Url:   "http://192.168.10.60:8009",
-			Debug: true,
-		}
-
-		err := client.Query(query)
-		fmt.Println("requst", client.LastRequest)
-		So(err, ShouldEqual, nil)
-
-		fmt.Println("response", client.LastResponse)
-
-		fmt.Printf("query.QueryResult:\n%v", query.QueryResult)
-
-	})
+	t.Parallel()
+	patterns := map[string]struct {
+		input    *QueryGroupBy
+		expected string
+	}{
+		"success": {
+			input: &QueryGroupBy{
+				DataSource:  "campaign",
+				Intervals:   []string{"2014-09-01T00:00/2020-01-01T00"},
+				Granularity: GranAll,
+				Filter:      FilterAnd(FilterJavaScript("hour", "function(x) { return(x >= 1) }"), nil),
+				LimitSpec:   LimitDefault(5),
+				Dimensions:  []DimSpec{"campaign_id"},
+				Aggregations: []Aggregation{
+					*AggRawJson(`{ "type" : "count", "name" : "count" }`),
+					*AggLongSum("impressions", "impressions"),
+				},
+				PostAggregations: []PostAggregation{
+					PostAggArithmetic(
+						"imp/count", "/",
+						[]PostAggregation{
+							PostAggFieldAccessor("impressions"),
+							PostAggRawJson(`{ "type" : "fieldAccess", "fieldName" : "count" }`)})},
+			},
+		},
+	}
+	for msg, p := range patterns {
+		t.Run(msg, func(t *testing.T) {
+			client := Client{
+				Url:        "http://localhost:8082",
+				HttpClient: &http.Client{},
+				Debug:      true,
+			}
+			err := client.Query(p.input, "")
+			fmt.Printf("requst: %s", client.LastRequest)
+			assert.NoError(t, err)
+			fmt.Printf("response: %s", client.LastResponse)
+			fmt.Printf("query.SubmitResult: %v", p.input.QueryResult)
+		})
+	}
 }
 
 func TestSearch(t *testing.T) {
-	// return
-	Convey("TestSearch", t, func() {
-		query := &QuerySearch{
-			DataSource:       "campaign",
-			Intervals:        []string{"2014-09-01T00:00/2020-01-01T00"},
-			Granularity:      GranAll,
-			SearchDimensions: []string{"campaign_id", "hour"},
-			Query:            SearchQueryInsensitiveContains(1313),
-			Sort:             SearchSortLexicographic,
-		}
-		client := Client{
-			Url:   "http://192.168.10.60:8009",
-			Debug: true,
-		}
-
-		err := client.Query(query)
-		So(err, ShouldEqual, nil)
-
-		fmt.Println("requst", client.LastRequest)
-		fmt.Println("response", client.LastResponse)
-
-		fmt.Printf("query.QueryResult:\n%v", query.QueryResult)
-
-	})
+	t.Parallel()
+	patterns := map[string]struct {
+		input    *QuerySearch
+		expected string
+	}{
+		"success": {
+			input: &QuerySearch{
+				DataSource:       "campaign",
+				Intervals:        []string{"2014-09-01T00:00/2020-01-01T00"},
+				Granularity:      GranAll,
+				SearchDimensions: []string{"campaign_id", "hour"},
+				Query:            SearchQueryInsensitiveContains(1313),
+				Sort:             SearchSortLexicographic,
+			},
+		},
+	}
+	for msg, p := range patterns {
+		t.Run(msg, func(t *testing.T) {
+			client := Client{
+				Url:        "http://localhost:8082",
+				HttpClient: &http.Client{},
+				Debug:      true,
+			}
+			err := client.Query(p.input, "")
+			fmt.Printf("requst: %s", client.LastRequest)
+			assert.NoError(t, err)
+			fmt.Printf("response: %s", client.LastResponse)
+			fmt.Printf("query.SubmitResult: %v", p.input.QueryResult)
+		})
+	}
 }
---
+++ retension_spec.go
@@ -0,0 +1,60 @@
+package godruid
+
+import (
+	"encoding/json"
+)
+
+type RetentionRulesSpec interface {
+	setup()
+	GetRawJSON() []byte
+	onResponse(content []byte) error
+}
+
+type RetentionRule struct {
+	Type             string            `json:"type"`
+	Period           string            `json:"period,omitempty"`
+	IncludeFuture    bool              `json:"includeFuture,omitempty"`
+	Interval         string            `json:"interval,omitempty"`
+	TieredReplicants *TieredReplicants `json:"tieredReplicants,omitempty"`
+}
+
+type RetentionRules struct {
+	rules               []*RetentionRule
+	RetentionRuleResult *RetentionRulesResult `json:"-"`
+	RawJSON             []byte                `json:"-"`
+}
+
+type RetentionRulesResult struct {
+	rules []*RetentionRule
+}
+
+type TieredReplicants struct {
+	Hot         int `json:"hot,omitempty"`
+	DefaultTier int `json:"_default_tier,omitempty"`
+}
+
+func (s *RetentionRules) setup()             {}
+func (s *RetentionRules) GetRawJSON() []byte { return nil }
+func (s *RetentionRules) onResponse(content []byte) error {
+	res := new(RetentionRulesResult)
+	err := json.Unmarshal(content, res)
+	if err != nil {
+		return err
+	}
+	s.RetentionRuleResult = res
+	s.RawJSON = content
+	return nil
+}
+
+func LoadForeverRetensionRule() *RetentionRule {
+	return &RetentionRule{
+		Type: "loadForever",
+	}
+}
+
+func DropBeforeByPeriodRetensionRule(period string) *RetentionRule {
+	return &RetentionRule{
+		Type:   "dropBeforeByPeriod",
+		Period: period,
+	}
+}
