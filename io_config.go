3bd74cb2bd9aca4c0be399dd9af8e6572e1b0ec0
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2021-01-21 09:14:22 +0900

    feat(ioconfig): add earlyMessageRejectPeriod

--- io_config.go
+++ io_config.go
@@ -3,17 +3,19 @@ package godruid
 type IOConfig interface{}
 
 type ioConfigKafka struct {
-	Type                      string              `json:"type"`
-	ConsumerProperties        *ConsumerProperties `json:"consumerProperties"`
-	Topic                     string              `json:"topic"`
-	LatestMessageRejectPeriod string              `json:"lateMessageRejectionPeriod"`
+	Type                        string              `json:"type"`
+	ConsumerProperties          *ConsumerProperties `json:"consumerProperties"`
+	Topic                       string              `json:"topic"`
+	LatestMessageRejectPeriod   string              `json:"lateMessageRejectionPeriod,omitempty"`
+	EarlyMessageRejectionPeriod string              `json:"earlyMessageRejectionPeriod,omitempty"`
 }
 
-func IOConfigKafka(topic, latestMessageRejectPeriod, bootstrapServers, saslMechanism, securityProtocal, sasLJAASConfig string) IOConfig {
+func IOConfigKafka(topic, latestMessageRejectPeriod, earlyMessageRejectionPeriod, bootstrapServers, saslMechanism, securityProtocal, sasLJAASConfig string) IOConfig {
 	return &ioConfigKafka{
-		Type:                      "kafka",
-		Topic:                     topic,
-		LatestMessageRejectPeriod: latestMessageRejectPeriod,
+		Type:                        "kafka",
+		Topic:                       topic,
+		LatestMessageRejectPeriod:   latestMessageRejectPeriod,
+		EarlyMessageRejectionPeriod: earlyMessageRejectionPeriod,
 		ConsumerProperties: &ConsumerProperties{
 			BootstrapServers: bootstrapServers,
 			SASLMechanism:    saslMechanism,
